e4af0c669aa857f781b4ccaa1749100f81418caf
정민석 <ms.jung@fanmaum.com>
2023-11-16 00:52:42 +0900

    Delete BlogController.kt

--- src/main/kotlin/com/example/blogkotlinstudy/controller/BlogController.kt
+++
@@ -1,9 +0,0 @@
-package com.example.blogkotlinstudy
-
-import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
-
-@RestController
-@RequestMapping("/api/blogs")
-class BlogController(private val blogService: BlogService) {
-}
