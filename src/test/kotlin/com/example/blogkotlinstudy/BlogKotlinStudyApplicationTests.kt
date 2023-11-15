f853dd34a2764867ea27929ebdd771acefbabd0c
정민석 <ms.jung@fanmaum.com>
2023-11-15 21:20:27 +0900

    add res

---
+++ src/test/kotlin/com/example/blogkotlinstudy/BlogKotlinStudyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.blogkotlinstudy
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogKotlinStudyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
