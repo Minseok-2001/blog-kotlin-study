4d9074254a46f3ccfc7b1ddf0275d55b910858ff
정민석 <ms.jung@fanmaum.com>
2023-11-29 22:56:46 +0900

    modify postDto

--- src/main/kotlin/com/example/blogkotlinstudy/controller/PostController.kt
+++ src/main/kotlin/com/example/blogkotlinstudy/controller/PostController.kt
@@ -3,7 +3,10 @@ package com.example.blogkotlinstudy.controller
 import com.example.blogkotlinstudy.dto.PostDto
 import com.example.blogkotlinstudy.service.PostService
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
---
+++ src/main/kotlin/com/example/blogkotlinstudy/dto/PostDto.kt
@@ -0,0 +1,8 @@
+package com.example.blogkotlinstudy.dto
+
+data class PostDto(
+	val id: Long,
+	val title: String,
+	val content: String,
+)
+
