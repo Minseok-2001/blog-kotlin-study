b1b220a05a2c6546869a71c627f6b13f70946c3f
정민석 <ms.jung@fanmaum.com>
2023-12-01 16:56:14 +0900

    chore: 불필요한 import를 제거합니다.

--- src/main/kotlin/com/example/blogkotlinstudy/controller/PostController.kt
+++ src/main/kotlin/com/example/blogkotlinstudy/controller/PostController.kt
@@ -5,7 +5,6 @@ import com.example.blogkotlinstudy.service.PostService
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.tags.Tag
-import io.swagger.v3.oas.annotations.tags.Tags
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
